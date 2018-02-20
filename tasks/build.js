4c615581708313a44f5d21e4cfd0468333c486dc
Philip Walton <philip@philipwalton.com>
2018-02-19 20:11:42 -0800

    Remove the icons tasks favoring inline icons

--- tasks/build.js
+++ tasks/build.js
@@ -3,11 +3,10 @@ const gulp = require('gulp');
 // Ensure referenced tasks are registered.
 require('./clean.js');
 require('./css.js');
-require('./icons.js');
 require('./javascript.js');
 require('./templates.js');
 
 gulp.task('build', gulp.series(
     'clean',
-    gulp.parallel('css', 'icons', 'javascript'),
+    gulp.parallel('css', 'javascript'),
     'templates'));
